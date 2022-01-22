72f64a312ccc265698489154f728e5ab611a14a8
unknown <skipperstrange@gmail.com>
2022-01-22 22:14:20 +0100

    Fresh start

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "~/components/Header.vue";
+export default {
+  name: 'IndexPage',
+  components: {
+    Header
+  }
+}
+</script>
